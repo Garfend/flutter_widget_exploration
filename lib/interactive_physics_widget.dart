738dc0aca225ce4099d10ec03b9e607886766949
abdelrahman <rain.forest5023@gmail.com>
2025-09-23 17:10:51 +0300

    add scaffold layout for interactive physics widget

--- lib/interactive_physics_widget.dart
+++ lib/interactive_physics_widget.dart
@@ -9,8 +9,25 @@ class InteractivePhysicsWidget extends StatefulWidget {
 }
 
 class _InteractivePhysicsWidgetState extends State<InteractivePhysicsWidget> {
+  final List<Color> allColors = [Colors.red, Colors.green, Colors.blue];
+  late List<Color> ballColors;
+  late List<Color> containerColors;
+  late Map<Color, bool> matched;
+  Color? draggingColor;
+  String? message;
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+    return Scaffold(
+      appBar: AppBar(title: const Text('Physics Drag & Drop')),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+          ],
+        ),
+      ),
+    );
+  }
