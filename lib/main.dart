3ed90ab34d212c392e91dcd13fdd79787d2fcda0
abdelrahman <rain.forest5023@gmail.com>
2025-09-23 17:09:15 +0300

    add bottom navigation bar to switch between screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_exploration/interactive_dismissable_list.dart';
+import 'package:flutter_widget_exploration/interactive_physics_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,16 +9,55 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const InterActiveDismisableLists(),
+      home: const HomeNavBar(),
+    );
+  }
+}
+
+class HomeNavBar extends StatefulWidget {
+  const HomeNavBar({super.key});
+
+  @override
+  State<HomeNavBar> createState() => _HomeNavBarState();
+}
+
+class _HomeNavBarState extends State<HomeNavBar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    InteractivePhysicsWidget(),
+    InterActiveDismisableLists(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports_baseball),
+            label: 'Physics',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Tasks',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
