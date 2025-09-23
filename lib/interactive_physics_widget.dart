15d3d48d4f9952f2120a07dc8b80490c8bafdef6
abdelrahman <rain.forest5023@gmail.com>
2025-09-23 16:38:18 +0300

    add interactive physics widget

---
+++ lib/interactive_physics_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class InteractivePhysicsWidget extends StatefulWidget {
+  const InteractivePhysicsWidget({super.key});
+
+  @override
+  State<InteractivePhysicsWidget> createState() =>
+      _InteractivePhysicsWidgetState();
+}
+
+class _InteractivePhysicsWidgetState extends State<InteractivePhysicsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
